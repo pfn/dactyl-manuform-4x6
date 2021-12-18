fb7c7238fe9ca2a4daa7290bcd1680d9c02e36bc
Perry Nguyen <pfnguyen@hanhuy.com>
2021-12-18 09:03:40 -0800

    refactor process_keymap into userspace

--- rules.mk
+++ rules.mk
@@ -1,3 +1,6 @@
+#TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
 SWAP_HANDS_ENABLE = yes
 DYNAMIC_TAPPING_TERM_ENABLE = yes
-SRC += process_keymap.c swap_hand.c
+SRC += swap_hand.c
